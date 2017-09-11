0beb17578067d7ac6162491c836ca7229457ee1b
kle10wka <evgeniymusic93@gmail.com>
2017-09-11 14:06:16 +0300

    add new tests

--- src/test/java/AllPosibleTests.java
+++ src/test/java/AllPosibleTests.java
@@ -167,4 +167,166 @@ public class AllPosibleTests {
 		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
 		Assert.assertTrue(currTime.getResult().equals("Good evening, World!"));
 	}
+	
+	@Test
+	public void test_FR_0_clock() {
+		Locale currLocale = new Locale("fr");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 0);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good night, World!"));
+	}
+
+	@Test
+	public void test_FR_23_o_clock(){
+		Locale currLocale = new Locale("fr");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 23);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good night, World!"));
+	}
+	
+	@Test
+	public void test_FR_6_o_clock(){
+		Locale currLocale = new Locale("fr");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 6);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good morning, World!"));
+	}
+	
+	@Test
+	public void test_FR_9_o_clock(){
+		Locale currLocale = new Locale("fr");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 9);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good day, World!"));
+	}
+	
+	@Test
+	public void test_FR_19_o_clock(){
+		Locale currLocale = new Locale("fr");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 19);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good evening, World!"));
+	}
+	
+	@Test
+	public void test_FR_5_o_clock(){
+		Locale currLocale = new Locale("fr");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 5);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good night, World!"));
+	}
+	
+	@Test
+	public void test_FR_8_o_clock(){
+		Locale currLocale = new Locale("fr");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 8);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good morning, World!"));
+	}
+	
+	@Test
+	public void test_FR_14_o_clock(){
+		Locale currLocale = new Locale("fr");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 14);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good day, World!"));
+	}
+	
+	@Test
+	public void test_FR_21_o_clock(){
+		Locale currLocale = new Locale("fr");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 21);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good evening, World!"));
+	}
+	
+	@Test
+	public void test_DE_0_clock() {
+		Locale currLocale = new Locale("de");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 0);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good night, World!"));
+	}
+
+	@Test
+	public void test_DE_23_o_clock(){
+		Locale currLocale = new Locale("de");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 23);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good night, World!"));
+	}
+	
+	@Test
+	public void test_DE_6_o_clock(){
+		Locale currLocale = new Locale("de");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 6);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good morning, World!"));
+	}
+	
+	@Test
+	public void test_DE_9_o_clock(){
+		Locale currLocale = new Locale("de");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 9);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good day, World!"));
+	}
+	
+	@Test
+	public void test_DE_19_o_clock(){
+		Locale currLocale = new Locale("de");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 19);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good evening, World!"));
+	}
+	
+	@Test
+	public void test_DE_5_o_clock(){
+		Locale currLocale = new Locale("de");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 5);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good night, World!"));
+	}
+	
+	@Test
+	public void test_DE_8_o_clock(){
+		Locale currLocale = new Locale("de");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 8);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good morning, World!"));
+	}
+	
+	@Test
+	public void test_DE_14_o_clock(){
+		Locale currLocale = new Locale("de");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 14);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good day, World!"));
+	}
+	
+	@Test
+	public void test_DE_21_o_clock(){
+		Locale currLocale = new Locale("de");
+		Calendar calendarTime = Calendar.getInstance(currLocale);
+		calendarTime.set(Calendar.HOUR_OF_DAY, 21);
+		TimeUtil currTime = new TimeUtil(calendarTime, currLocale);
+		Assert.assertTrue(currTime.getResult().equals("Good evening, World!"));
+	}
 }
